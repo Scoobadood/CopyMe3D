05db008e27e661cd37457793a8a1b041f43ee176
Dave Durbin <scoobadood@me.com>
2017-07-13 08:59:16 +1000

    Feature: Added basic code to find and open device.

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,10 @@
 #include <iostream>
 
+#include <libfreenect2/libfreenect2.hpp>
+#include <libfreenect2/frame_listener_impl.h>
+#include <libfreenect2/registration.h>
+#include <libfreenect2/packet_pipeline.h>
+
 #include "CopyMe3DConfig.h"
 
 /*
@@ -8,6 +13,24 @@
 */
 
 int main( int argc, const char * argv[] ) {
+	using namespace libfreenect2;
 
+	// Enumerate Kinect devices
+	Freenect2 freenect2;
+	if( freenect2.enumerateDevices( ) != 0 ) {
+		std::string deviceSerial = freenect2.getDefaultDeviceSerialNumber( );
+	
+		PacketPipeline * pipeline = new CpuPacketPipeline( );
+
+		Freenect2Device * dev = freenect2.openDevice( deviceSerial, pipeline );
+
+		// Close device
+		dev -> close( );
+	}
+
+	// Else no devices found
+	else {
+		std::cout << "No devices found" << std::endl;
+	}
 
 }
