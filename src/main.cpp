7be2a64b79cad3de3ab56c8a5b460c77ca68caac
Dave Durbin <scoobadood@me.com>
2017-07-13 18:40:37 +1000

    Feature: Capture RGB, Depth and IR frames from Kinect.
    Captured a single frame of each. Does nothing with them.

--- src/main.cpp
+++ src/main.cpp
@@ -19,11 +19,38 @@ int main( int argc, const char * argv[] ) {
 	Freenect2 freenect2;
 	if( freenect2.enumerateDevices( ) != 0 ) {
 		std::string deviceSerial = freenect2.getDefaultDeviceSerialNumber( );
-	
 		PacketPipeline * pipeline = new CpuPacketPipeline( );
-
 		Freenect2Device * dev = freenect2.openDevice( deviceSerial, pipeline );
 
+		int types = Frame::Color | Frame::Ir | Frame::Depth;
+  		SyncMultiFrameListener listener(types);
+
+  		FrameMap frames;
+  		dev->setColorFrameListener(&listener);
+  		dev->setIrAndDepthFrameListener(&listener);
+
+		// Start the device
+		if( dev -> start( ) ) {
+			// Wait for frames
+			if ( !listener.waitForNewFrame( frames, 10*1000 ) )  {// 10 sconds
+				Frame * rgb = frames[ Frame::Color];
+				Frame * ir = frames[ Frame::Ir];
+				Frame * depth = frames[ Frame::Depth];
+
+				listener.release( frames );
+			}
+
+			// No frames in time
+			else {
+				std::cout << "timeout!" << std::endl;
+     		}
+			dev->stop();
+		}
+
+		// Device didn;t start
+		else {
+			std::cout << "Couldn't start device" << std::endl;
+		}
 		// Close device
 		dev -> close( );
 	}
